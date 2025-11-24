45ecaf0d73efdbd2543eac0364b369fb481b361f
chen <kwdiwt@sina.com>
2025-11-24 14:35:43 +0800

    feat: uncomment verify

--- src/autoweb/tools/verify.py
+++ src/autoweb/tools/verify.py
@@ -9,14 +9,14 @@ from .config import config
 async def verify():
     async with aiohttp.ClientSession(json_serialize=ujson.dumps) as session:
         while True:
-            # async with session.post(
-            #     config.SIBERIAN_URL,
-            #     json={
-            #         "siberian": config.SIBERIAN_KEY,
-            #         "deviceCode": config.DEVICE_CODE,
-            #     },
-            # ) as response:
-            #     res = await response.json()
-            #     if res["code"] != 200:
-            #         raise Exception(res)
+            async with session.post(
+                config.SIBERIAN_URL,
+                json={
+                    "siberian": config.SIBERIAN_KEY,
+                    "deviceCode": config.DEVICE_CODE,
+                },
+            ) as response:
+                res = await response.json()
+                if res["code"] != 200:
+                    raise Exception(res)
             await sleep(180)
